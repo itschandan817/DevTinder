9c4dca148be1b0d298c91ef9746a2cfe17376141
Chandan kumar <itschandan817@gmail.com>
2024-12-09 12:36:46 +0530

    create express server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res)=>{
+    res.send("Hello from the Server");
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is successfully listing on port 3000")
+});
